185784875230b64b9edb2df1f7ce41d2fbc8ca75
Next Scripts® <docforbusiness@gmail.com>
2024-06-28 18:14:34 +0200

    add available frameworks and allowed jobs
    add available frameworks and allowed jobs

--- config.lua
+++ config.lua
@@ -1,1 +1,13 @@
 Config = {}
+
+--[[
+    "esx" and "qb" are available
+]]
+Config.Framework = "esx"
+
+--- Jobs that can receive dispatch notification
+Config.AllowedJobs = {
+    ["police"] = {
+
+    },
+}
