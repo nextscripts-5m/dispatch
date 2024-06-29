bedee517436ef91be8bd136ca12389d259f5d622
Next Scripts® <docforbusiness@gmail.com>
2024-06-29 12:03:06 +0200

    add client framework functions
    add client framework functions

---
+++ client/framework.lua
@@ -0,0 +1,30 @@
+TriggerServerCallback = function (name, callback, ...)
+
+    if Framework == "ESX" then
+        ESX.TriggerServerCallback(name, callback, ...)
+
+    elseif Framework == "QB" then
+        QBCore.Functions.TriggerCallback(name, callback, ...)
+    end
+end
+
+---Show notification depending on framework
+---@param message string the message
+---@param type string info, success, error
+ShowNotification = function (message, type)
+    type = type and type or "info"
+    if Framework == "ESX" then
+        ESX.ShowNotification(message, type)
+
+    elseif Framework == "QB" then
+        QBCore.Functions.Notify(message, type, 5000)
+    end
+end
+
+GetJobFramework = function ()
+    if Framework == "ESX" then
+        return ESX.PlayerData.job.name
+    elseif Framework == "QB" then
+        return QBCore.Functions.GetPlayerData().job.name
+    end
+end
