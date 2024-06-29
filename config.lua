f59963e520b959d00e5e8ae958cd1ea1a0cf0c45
Next Scripts® <docforbusiness@gmail.com>
2024-06-29 12:02:05 +0200

    add config for locales
    add config for locales

--- config.lua
+++ config.lua
@@ -5,6 +5,10 @@ Config = {}
 ]]
 Config.Framework = "esx"
 
+-- "en" or "it" are now available
+Config.Locales = "en"
+Language = Lang[Config.Locales]
+
 --- Jobs that can receive dispatch notification
 Config.AllowedJobs = {
     ["police"] = {
