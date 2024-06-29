c894b8e62883b0524fe717252ea4a28f20da4448
Next Scripts® <docforbusiness@gmail.com>
2024-06-29 12:01:50 +0200

    locales
    locales

---
+++ locales.lua
@@ -0,0 +1,14 @@
+Lang = {}
+
+Lang = {
+    ["it"] = {
+        ["new-dispatch"] = "Hai una nuova segnalazione!",
+        ["dispatch-title"] = "Lista delle notifiche",
+        ["dispatch-people"] = "Persone",
+    },
+    ["en"] = {
+        ["new-dispatch"] = "You have a new report!",
+        ["dispatch-title"] = "Dispatch notifies",
+        ["dispatch-people"] = "People",
+    }
+}
