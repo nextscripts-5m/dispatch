764e2dd25b5dbac830f3e394ad9046430d224531
Next Scripts® <docforbusiness@gmail.com>
2024-06-29 12:02:55 +0200

    add create/update dispatch notify event for client + command for opening dispatch notifies
    add create/update dispatch notify event for client + command for opening dispatch notifies

--- client/client.lua
+++ client/client.lua
@@ -1,11 +1,47 @@
+local dispatchList = DispatchList:new({})
+
 ---Create the dispatch notification (Client)
 ---@param title string Dispatch Title
 ---@param description string Dispatch description
 ---@param jobName string Receiver job
-CreateDispatchNotify = function (title, description, jobName)
-    TriggerServerEvent("nx_dispatch:CreateDispatchNotify", title, description, jobName)
+---@param coords vector3 Gps Coords
+CreateDispatchNotify = function (title, description, jobName, coords)
+    TriggerServerEvent("nx_dispatch:CreateDispatchNotify", title, description, jobName, coords)
 end
 exports("CreateDispatchNotify", CreateDispatchNotify)
 
 
---- TODO: register event for creating UI
+--[[
+    Events
+]]
+
+---@param dispatchNotification Notify
+RegisterNetEvent("nx_dispatch:SendDispatchNotification", function (dispatchNotification)
+    ShowNotification(Language["new-dispatch"])
+    dispatchList:addNotification(dispatchNotification)
+end)
+
+
+---@param dispatchNotification Notify
+RegisterNetEvent("nx_dispatch:updateDispatch", function (dispatchNotification)
+    dispatchList.notifications[dispatchNotification.id] = dispatchNotification
+end)
+
+--[[
+    Commands
+]]
+
+RegisterCommand("openDispatch", function (source, args, raw)
+    local contextID = RegisterContext(dispatchList.notifications)
+    lib.showContext(contextID)
+end)
+RegisterKeyMapping('openDispatch', 'Open Dispatch List', 'keyboard', 'l')
+
+--[[
+    Test
+    !DO NOT UNCOMMENT THIS!
+]]
+
+RegisterCommand("cc", function (source, args, raw)
+    CreateDispatchNotify("Title 1", "Description", "police", GetEntityCoords(PlayerPedId()))
+end)
