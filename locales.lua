655be78b5acddffeb9dde5f82bac39e8657f4b0b
Next Scripts® <docforbusiness@gmail.com>
2024-06-29 14:51:32 +0200

    add locales
    add locales

--- locales.lua
+++ locales.lua
@@ -5,10 +5,18 @@ Lang = {
         ["new-dispatch"] = "Hai una nuova segnalazione!",
         ["dispatch-title"] = "Lista delle notifiche",
         ["dispatch-people"] = "Persone",
+        ["go"] = "Vai",
+        ["delete"] = "Elimina Notifica",
+        ["already-seen"] = "Segna come già letta!",
+        ["not-seen"] = "Segna come NON letta!",
     },
     ["en"] = {
         ["new-dispatch"] = "You have a new report!",
         ["dispatch-title"] = "Dispatch notifies",
         ["dispatch-people"] = "People",
+        ["go"] = "Go!",
+        ["delete"] = "Delete Notification",
+        ["already-seen"] = "Mark as already read!",
+        ["not-seen"] = "Mark as NOT read!",
     }
 }
