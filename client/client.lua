27393ddbfa19e7fefe76f4e2265fdf082a904ed1
Next Scripts® <docforbusiness@gmail.com>
2024-07-03 15:59:07 +0200

    add NUICallbacks for removing, setgps and updatestate and close
    add NUICallbacks for removing, setgps and updatestate and close

--- client/client.lua
+++ client/client.lua
@@ -19,6 +19,10 @@ exports("CreateDispatchNotify", CreateDispatchNotify)
 RegisterNetEvent("nx_dispatch:SendDispatchNotification", function (dispatchNotification)
     ShowNotification(Language["new-dispatch"])
     MyDispatchList:addNotification(dispatchNotification)
+    SendNUIMessage({
+        update = true,
+        notification = dispatchNotification
+    })
 end)
 
 
@@ -28,14 +32,49 @@ RegisterNetEvent("nx_dispatch:updateDispatch", function (dispatchNotification)
 end)
 
 --[[
+    NUI Callbacks
+]]
+
+RegisterNUICallback("closeUI", function (data, cb)
+    print(data.message)
+    SetNuiFocus(false, false)
+end)
+
+RegisterNUICallback("updateState", function (data, cb)
+    TriggerServerEvent("nx_dispatch:UpdateDispatchcNotifyState", data.id, data.state)
+end)
+
+RegisterNUICallback("setGps", function (data, cb)
+    SetNewWaypoint(data.x, data.y)
+    TriggerServerEvent("nx_dispatch:UpdateDispatchNotifyCounter", data.id)
+end)
+
+RegisterNUICallback("removeDispatch", function (data, cb)
+    MyDispatchList:removeNotification(data.id)
+    TriggerServerEvent("nx_dispatch:UpdateDispatchNotifyPlayer", data.id)
+end)
+
+--[[
     Commands
 ]]
 
 RegisterCommand("openDispatch", function (source, args, raw)
-    local contextID = RegisterContext(MyDispatchList.notifications)
-    lib.showContext(contextID)
+
+    if Config.UI == "lib" then
+        local contextID = RegisterContext(MyDispatchList.notifications)
+        lib.showContext(contextID)
+    end
+
+    if Config.UI == "custom" then
+        SendNUIMessage({
+            show = true,
+            dispatches = MyDispatchList,
+        })
+        SetNuiFocus(true, true)
+    end
 end)
 RegisterKeyMapping('openDispatch', 'Open Dispatch List', 'keyboard', 'l')
+
 
 --[[
     Test
